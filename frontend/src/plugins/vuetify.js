286a0e565b52a2819069665f65a2cad3f99679f0
Matt Dean <mdean400@gmail.com>
2021-03-03 03:48:58 -0700

    Give information about what I had to modify from what was produced by vue add vuetify

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,4 +1,7 @@
 import Vue from "vue";
+
+// had to modify this from the original produced by the "vue add vuetify" command because the original did not work
+// import Vuetify from "vuetify/lib/framework";
 import Vuetify from "vuetify";
 
 Vue.use(Vuetify);
