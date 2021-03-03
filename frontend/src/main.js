85be1dec794d3209d8ff957dfa76f492e96bcd93
Matt Dean <mdean400@gmail.com>
2021-03-03 03:49:15 -0700

    More modification information

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+
+// had to add this after the "vue add vuetify" command because the CSS styles are not loaded without it
 import "vuetify/dist/vuetify.min.css";
 
 Vue.config.productionTip = false;
